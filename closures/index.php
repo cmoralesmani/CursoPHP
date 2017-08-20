2463f98356eef82a5227b2fa9931e3ff420755cc
Cesar Morales <camd.jun@gmail.com>
2017-08-19 23:13:00 -0600

    Ejemplo de uso de clases e instancias Programación Orientada a Objeto

--- closures/index.php
+++ closures/index.php
@@ -1,10 +1,21 @@
 <?php
 
-$var2 = 1;
+// $var2 = 1;
 
-$var = function (){
-    echo 'This is a closure';
-    echo 'Value: ' . $var2;
+// $var = function () use($var2){
+//     echo 'This is a closure';
+//     echo 'Value: ' . $var2;
+// };
+
+// $var();
+
+$x = 3;
+$numbers = [1,2,3,4,5];
+
+$closure = function($n) use($x){
+    return $n * $x;
 };
 
-$var();
+$result = array_map($closure, $numbers);
+
+var_dump($result);
